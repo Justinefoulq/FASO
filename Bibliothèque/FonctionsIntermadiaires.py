3e29f707f3c4fc47ff077b23abc7688ae99bbdc3
Justinefoulq <43746283+Justinefoulq@users.noreply.github.com>
2018-11-05 16:33:57 +0100

    Update FonctionsIntermadiaires.py

--- Bibliothèque/FonctionsIntermadiaires.py
+++ Bibliothèque/FonctionsIntermadiaires.py
@@ -1,1 +1,17 @@
+import Poids
+import RFID
+import HautParleur
 
+def PersonneDetecte():
+  poidPlanche=4
+  return Poids.PoidsDetect()> ( 5+ poidPlanche)
+
+def LancerAlarm():
+  if PersonneDetecte() && !(RFID.BadgeDetecte):
+    HautParleur.AllumerAlarm()
+    
+def ArreterAlarm():
+  if !(PersonneDetecte()) || (RFID.BadgeDetecte):
+    HautParleur.EteindreAlarm()
+  
+  

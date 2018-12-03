91273f700e646e1f8684a5fe378799ef8d10ba73
FelixPotie <43473053+FelixPotie@users.noreply.github.com>
2018-12-03 13:45:42 +0100

    Update Admin.py

--- Admin.py
+++ Admin.py
@@ -1,8 +1,31 @@
+import serial
 
-def creerAdmin ()
-def suprAdmin ()
+ser = serial.Serial('/dev/ttyACMO',9600)
+admin=[]
 
-#Retourne la liste des administrateur
-def ListeAdmin()
+def creerAdmin ():
+  admin= admin + [ser.readline()]
+  return admin
   
+def supprAdmin ():
+  for i in admin:
+    if i == ser.readline():
+      i=nil
+  return admin
 
+
+def ListeAdmin():
+  return admin
+
+def QueFaire():
+  print("Voulez vous Ajouter('A'), Supprimer('S'), ou Voire('V') les admins?")
+  action=readline()
+  if action = 'A':
+    admin=creerAdmin()
+  else if action ='S':
+    admin=supprAdmin()
+  else 
+    for i in admin:
+      println(i)
+
+Quefaire()      

6e547c7f10405ec2e123b92e45cf14f6edab5c80
FelixPotie <43473053+FelixPotie@users.noreply.github.com>
2018-12-03 13:12:11 +0100

    Update RFID.py

--- Bibliothèque/RFID.py
+++ Bibliothèque/RFID.py
@@ -1,3 +1,12 @@
+import serial
+import Admin.py
 
-def BadgeDetecte():
+def BadgeDetecte(): 
+  ser = serial.Serial('/dev/ttyACMO',9600)
+  CodeCarte = ser.readline()
+  admin = Admin.ListeAdmin()
+  for i in admin :
+    if i == CodeCarte :
+      return True
+  return False
   

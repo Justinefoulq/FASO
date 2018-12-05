01e09079a0cf3d63645079a8aa3b1cb99c6d0138
FelixPotie <43473053+FelixPotie@users.noreply.github.com>
2018-12-05 19:16:58 +0100

    Update RFID.py

--- Bibliothèque/RFID.py
+++ Bibliothèque/RFID.py
@@ -1,17 +1,20 @@
 import serial
+import pickle
 
-ser = serial.Serial('/dev/ttyACMO',9600)
+ser = serial.Serial('/dev/ttyACM0',9600)
 
 def BadgeDetecte():
   res=True
+  i=0
   CodeCarte = ser.readline()
-  Fichier=open('ListeAdmin.txt','rb')
+  Fichier=open('../ListeAdmin.txt','rb')
   admin=pickle.load(Fichier)
   Fichier.close
   while i< len(admin) and res:
-    if admin[i] == Codecarte:
+    if admin[i] == CodeCarte:
       res = False
     i=i+1
   return not(res)
 
 print(BadgeDetecte())
+

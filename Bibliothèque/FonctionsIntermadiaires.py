85c7284d24f34c5edfc9de4ab267229a2c035a3c
FelixPotie <43473053+FelixPotie@users.noreply.github.com>
2018-12-10 14:51:40 +0100

    Update FonctionsIntermadiaires.py

--- Bibliothèque/FonctionsIntermadiaires.py
+++ Bibliothèque/FonctionsIntermadiaires.py
@@ -1,22 +1,22 @@
-import Poids
-import RFID
+#import Poids.py
+import RFID.py
 
 def PersonneDetecte():
   poidPlanche=4
   return Poids.PoidsDetect()> ( 5+ poidPlanche)
 
 def LancerAlarm():
-	while True:
-		while PersonneDetecte() and not(BadgeDetecte()):
-			try:
-				grovepi.digitalWrite(buzzer,1)
-				print('start')
-				time.sleep(0.5)
-				grovepi.digitalWrite(buzzer,0)
-				print('stop')
-				time.sleep(0.5)
-			except KeyboardInterrupt:
-				grovepi.digitalWrite(buzzer,0)
-				break
-		grovepi.digitalWrite(buzzer,0)
-
+  while True:
+    #while PersonneDetecte() and not(BadgeDetecte()):
+    while not(RFID.BadgeDectecte())
+    try:
+      grovepi.digitalWrite(buzzer,1)
+      print('start')
+      time.sleep(0.5)
+      grovepi.digitalWrite(buzzer,0)
+      print('stop')
+      time.sleep(0.5)
+    except KeyboardInterrupt:
+      grovepi.digitalWrite(buzzer,0)
+      break
+    grovepi.digitalWrite(buzzer,0)

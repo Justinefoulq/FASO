5ff0dc5ab492064f14c20d97f483fcee343d2d1a
FelixPotie <43473053+FelixPotie@users.noreply.github.com>
2018-12-10 14:50:27 +0100

    Update RFID.py

--- Bibliothèque/RFID.py
+++ Bibliothèque/RFID.py
@@ -1,11 +1,20 @@
 import serial
 import pickle
+import time
 
 ser = serial.Serial('/dev/ttyACM0',9600)
 
 def BadgeDetecte():
   res=True
   i=0
+
+
+ # int tempsdebut = time()
+ # int tempsici =  time() - tempsdebut
+ # while tempsici()<5 || CodeCarte = ser.readline()
+ #   tempsici=time() - tempsdebut
+
+  
   CodeCarte = ser.readline()
   Fichier=open('../ListeAdmin.txt','rb')
   admin=pickle.load(Fichier)
@@ -16,5 +25,10 @@ def BadgeDetecte():
     i=i+1
   return not(res)
 
+
+while True:
+  BadgeDetecte()  
+
 print(BadgeDetecte())
+
 
